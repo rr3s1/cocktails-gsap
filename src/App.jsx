2ebbef88c1044269dbb6e99cc937e644f2063a39
aessary <raressilviulazar@gmail.com>
2025-07-01 07:47:15 +0100

    Art Section - Scroll-Based Mask Animation

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import gsap from 'gsap'
 import {ScrollTrigger, SplitText} from "gsap/all";
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
+import Cocktails from "./components/Cocktails.jsx";
+import About from "./components/About.jsx";
+import Art from "./components/Art.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -13,7 +16,9 @@ const App = () => {
         <main>
             <Navbar/>
             <Hero />
-            <div className="h-dvh bg-black"></div>
+            <Cocktails />
+            <About />
+            <Art />
         </main>
     )
 }
